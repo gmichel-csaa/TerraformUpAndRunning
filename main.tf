d699b968aeaf1fbee4099d459b0ebe0887a70f60
Terraform Lab user <example@email.com>
2018-11-04 20:37:01 -0200

    Added ELB to the Auto Scale Group - needs troubleshooting to work

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "aws_availability_zones" "all" {}
 #
 # set a launch configuration resource for instances
 #
-resource "aws_launch_configuration" "LCExample" {
+resource "aws_launch_configuration" "LaunchConfigurationExample" {
   image_id	= "ami-40d28157"
   instance_type = "t2.micro"
 
@@ -69,8 +69,11 @@ resource "aws_security_group" "WebServerSecurityGroup" {
 # Auto Scaling Group #
 #
 resource "aws_autoscaling_group" "AutoScalingGroupExample" {
-  launch_configuration = "${aws_launch_configuration.LaunchConfigurationExample.id}"
-  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  launch_configuration	= "${aws_launch_configuration.LaunchConfigurationExample.id}"
+  availability_zones	= ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers	= ["${aws_elb.ElasticLoadBalancerExample}"]
+  health_check_type	= "ELB"
 
   min_size = 2
   max_size = 10
@@ -83,15 +86,56 @@ resource "aws_autoscaling_group" "AutoScalingGroupExample" {
   }
 }
 #
+# Elastic Load Balancer #
+resource "aws_elb" "ElasticLoadBalancerExample" {
+  name			= "terraform-asg.example"
+  availability_zones	= ["${data.aws_availability_zones.all.names}"]
+  security_groups	= ["${aws_security_group.elb.id}"] ## this associates the ELB with its security group
+ 
+  listener {
+    lb_port		= 80
+    lb_protocol		= "http"
+    instance_port	= "${var.server_port}"
+    instance_protocol	= "http"
+  }
+
+  health_check {
+    healthy_threshold	= 2
+    unhealthy_threshold = 2
+    timeout		= 3
+    inverval		= 30
+    target		= "HTTP:${var.server_port}/"
+  }
+}
+#
+## Security group for the elastic load balancer ##
+#
+resource "aws_security_group" "elb" {
+  name = "terraform_example-elb"
+
+  ingress {
+    from_port		= 80
+    to_port		= 80
+    protocol		= "tcp"
+    cidr_blocks		= ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port		= 0
+    to_port		= 0
+    protocol		= 1
+    cidr_blocks 	= ["0.0.0.0/0"]
+  }
+}
+#
 #
 ### Outputs ###
 #
 #
-# Instance public IP address #
+# Instance public IP address / ELB DNS name #
 #
-###output "Public IP" {
-###  value = "${aws_instances.LaunchConfigurationExample.public_ip}"
-###}
+output "Public IP" {
+  value = "${aws_elb.ElassticLoadBalancerExample.dns_name}"
+}
 #
 # end #
-
